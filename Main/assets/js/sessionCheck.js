379b463dd9b751336171160142d1fde58fefd175
Samuel K. Kihoon <60698168+SamuelKihoon@users.noreply.github.com>
2024-08-28 23:55:13 +0900

    Fixed bug in sessionCheck.js

--- Main/assets/js/sessionCheck.js
+++ Main/assets/js/sessionCheck.js
@@ -21,15 +21,28 @@
  
               document.getElementById("usertype").innerHTML = snapshot.val().unit //텍스트 변경, 유저타입
 
-              document.getElementById("usernameindex").innerHTML = snapshot.val().name //텍스트 변경, 유저타입
+              
 
-  
+
+            try {
+              // 오류가 발생할 가능성이 있는 코드
+              document.getElementById("usernameindex").innerHTML = snapshot.val().name //텍스트 변경, 유저타입
+          } catch (error) {
+              console.error("An error occurred:", error); // 오류를 콘솔에 출력
+              // 필요에 따라 오류 처리 코드를 여기에 작성
+          }
+          
+
+              //alert("KiHOONOK v2 보안테스트 : 세션체크 로그인됌 - 테스트 메시지입니다")
+
               name = snapshot.val().name;   //유저 닉네임은 계속 쓸거기 때문에 전역변수로 할당
               loginUserKey = snapshot.key;  //로그인한 유저의 key도 계속 쓸 것이기 때문에 전역변수로 할당
               userInfo = snapshot.val(); //snapshot.val()에 user 테이블에 있는 해당 개체 정보가 넘어온다. userInfo에 대입!
  
 
-                boardList();
+              
+
+                boardListFun();
 
 
               //이부분까지 index.html에 해당하는 로직 이후에 엘리멘트 id로 mypage인지 메인 페이지인지 구분
@@ -66,6 +79,7 @@
               //alert(userInfo.userid);  //uid는 db에서 user 테이블의 각 개체들의 pk 인데, 이 값은 인증에서 생성된 아이디의 pk 값과 같다. *중요!
               //리스트 호출
 
+ 
 
               return true
               }
